54e351bbb0d8a55df796102284fab53f3589d7ef
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-01-02 14:42:46 -0600

    fixed mobile stuff

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = ({ toggleTheme, isDarkMode }) => {
   const [scrolled, setScrolled] = useState(false);
+  const [ isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,20 +26,60 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
       <div className="nav-logo">
         <span>OB</span>
       </div>
-      <div className="nav-links">
-        <Link to="home" spy={true} smooth={true} duration={500} className="nav-item">
+      <div className="hamburger" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        {/* Add isOpen class to nav-links */}
+      <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+        <Link 
+          to="home" 
+          spy={true} 
+          smooth={true} 
+          duration={500} 
+          className="nav-item"
+          onClick={() => setIsOpen(false)} // Close menu when clicking a link
+        >
           Home
         </Link>
-        <Link to="about" spy={true} smooth={true} duration={500} className="nav-item">
+        <Link 
+          to="about" 
+          spy={true} 
+          smooth={true} 
+          duration={500} 
+          className="nav-item"
+          onClick={() => setIsOpen(false)}
+        >
           About
         </Link>
-        <Link to="projects" spy={true} smooth={true} duration={500} className="nav-item">
+        <Link 
+          to="projects" 
+          spy={true} 
+          smooth={true} 
+          duration={500} 
+          className="nav-item"
+          onClick={() => setIsOpen(false)}
+        >
           Projects
         </Link>
-        <Link to="resume" spy={true} smooth={true} duration={500} className="nav-item">
+        <Link 
+          to="resume" 
+          spy={true} 
+          smooth={true} 
+          duration={500} 
+          className="nav-item"
+          onClick={() => setIsOpen(false)}
+        >
           Resume
         </Link>
-        <Link to="contact" spy={true} smooth={true} duration={500} className="nav-item">
+        <Link 
+          to="contact" 
+          spy={true} 
+          smooth={true} 
+          duration={500} 
+          className="nav-item"
+          onClick={() => setIsOpen(false)}
+        >
           Contact
         </Link>
         <button onClick={toggleTheme} className="theme-toggle">
