e31b59f4d0d6f85c82d1f016e6782083115e257e
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-01-20 23:53:03 -0500

    me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -250,7 +250,7 @@ const About = () => {
     { name: 'Java', level: 90 },
     { name: 'C', level: 60 },
     { name: 'SQL', level: 60 },
-    { name: 'Database Management', level: 75 }
+    { name: 'D atabase Management', level: 75 }
   ];
 
 
