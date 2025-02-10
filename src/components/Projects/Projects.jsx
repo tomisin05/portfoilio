022677e6c65e751d0afc207c37c7eac663cd096d
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-02-10 07:35:49 -0500

    added demo video for portraitify

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -138,7 +138,7 @@ const Projects = () => {
         "description": "Portraitify is an innovative web application that transforms everyday photos into stunning professional portraits using advanced AI technology. Users can upload their images, customize their look with various styles, settings, and lighting options, and generate high-quality portraits tailored to their preferences. The platform also features a gallery for users to view and download their generated portraits, making it perfect for enhancing professional profiles and personal branding.",
         "image": "/projects/Portraitify/Portraitify.png",
         "github": "https://github.com/tomisin05/portraitify", 
-        "demo": "", // "/projects/portraitify/demo.mp4",
+        "demo": "/projects/portraitify/demo.mp4",
         "siteUrl": "https://portraitify.vercel.app/", 
         "technologies": ["React", "Firebase", "Tailwind CSS", "Vite", "Stripe", "Flux AI", "JZIP"],
         "categories": ["web", "ai"]
